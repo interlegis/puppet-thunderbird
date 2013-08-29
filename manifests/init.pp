32db30363c9cfe30ccc27350fab963a900e08ad0
Fabio Rauber <fabiorauber@gmail.com>
2013-08-29 08:54:14 -0300

    Second commit

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,27 @@
 #init.pp
 
-class thunderbird (
+class thunderbird (	$maildomain = undef,		#Required
+			$accountdomain = undef,		#Required
+			$imapserver = undef,		#Required
+			$organization = undef,		#Required
+			$smtpserver = undef,		#Required
+			$ldapuri = undef,		#Optional
+			$caldavserver = undef,		#Optional
+			$caltimezone = undef,		#Required if caldavserver is set
 		) {
+	
+	package { "thunderbird": ensure => latest }
 
+	file { "/etc/thunderbird/syspref.js":
+                owner => root, group => root, mode => 444,
+                content => template('thunderbird/syspref.js.erb'),
+		require => Package["thunderbird"],
+        }
+	file { "/usr/lib/thunderbird/thunderbird.cfg":
+                owner => root, group => root, mode => 444,
+                content => template('thunderbird/thunderbird.cfg.erb'),
+		require => File["/etc/thunderbird/syspref.js"],
+	}
 
 
 }
