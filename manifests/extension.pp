fb74d4006c16520cef9e8e9fc35d107fffa349f4
Fabio Rauber <fabiorauber@gmail.com>
2013-09-02 09:38:44 -0300

    Fixed bugs

--- manifests/extension.pp
+++ manifests/extension.pp
@@ -9,10 +9,12 @@ define thunderbird::extension (	$src = undef,
 		notify => Exec["extract extension $name"],
 	}
 	
-	package { ["xmlstarlet","unzip"]: ensure => installed }
+	if ! defined(Package['xmlstarlet']) { package { "xmlstarlet": ensure => installed } }
+	if ! defined(Package['unzip']) { package {"unzip": ensure => installed } }
 
 	exec { "extract extension $name":
 		refreshonly => true,
+		cwd => $dst,
 		command => "unzip ${name}.xpi -d `unzip -qc ${name}.xpi install.rdf | xmlstarlet sel -N rdf=http://www.w3.org/1999/02/22-rdf-syntax-ns# -N em=http://www.mozilla.org/2004/em-rdf# -t -v \"//rdf:Description[@about='urn:mozilla:install-manifest']/em:id\"`",
 		require => Package[ ["unzip","xmlstarlet"] ],
 	}
